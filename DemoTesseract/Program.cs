3955fbdb71403bb0949d29cd4a60ddabe9b58e5e
Oscar Samaniego <oscar_sm@hotmail.com>
2017-09-25 17:03:29 -0500

    cargar imagen y configuración.

--- DemoTesseract/Program.cs
+++ DemoTesseract/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tesseract;
+
 
 namespace DemoTesseract
 {
@@ -10,6 +12,33 @@ namespace DemoTesseract
     {
         static void Main(string[] args)
         {
+            var testImage = "./captcha.jpg";
+
+            try
+            {
+                using (var engine = new TesseractEngine(@"tessdata", "eng", EngineMode.Default))
+                {
+                    using (var img = Pix.LoadFromFile(testImage))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var texto = page.GetText();
+
+                            Console.WriteLine("Precisión : {0}", page.GetMeanConfidence());
+                            Console.WriteLine("Texto: {0}", texto);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+                throw;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
